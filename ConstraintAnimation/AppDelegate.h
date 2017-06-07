4477fbdd7da681a47e1bec2441a8c86f24cffcbf
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-06-07 15:19:02 -0300

    Initial Commit

---
+++ ConstraintAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConstraintAnimation
+//
+//  Created by Cast on 6/7/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConstraintAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConstraintAnimation
+//
+//  Created by Cast on 6/7/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
